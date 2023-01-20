eccac201abbc2d5dbda171c1a614b4a8cca8e27b
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-01-20 13:51:32 -0500

    Completed and Passed up to and including exercise 16

--- exercises/e16.js
+++ exercises/e16.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 16
 // Please, read the exercise-info/flat.md to get the initial data of what is the expected result of this exercise.
 // Array example: flatArraysData in /data/data.js
@@ -7,10 +6,20 @@
 
 export function flatArrays(array) {
   // Your code goes here...
-
+  const newArray = [];
+  function helperFunction(array) {
+    for (let i = 0; i < array.length; i++) {
+      let element = array[i];
+      if (Array.isArray(element)) {
+        helperFunction(element);
+      } else {
+        newArray.push(element);
+      }
+    }
+  }
+  helperFunction(array);
+  return newArray;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-16"
